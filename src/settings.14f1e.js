8fe4a14fb0f53aa2ef7d554faef1dedfebbb36eb
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2025-01-23 16:49:46 +0200

    fix

---
+++ src/settings.14f1e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:["main"],launchScene:"db://assets/Game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"468ae",resources:"08a3e",main:"13448"}};
