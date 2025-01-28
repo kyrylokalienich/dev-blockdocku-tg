ebd397b5f39c85059415b64294e26d5ee35a96ec
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2025-01-28 11:59:59 +0200

    f6

--- src/settings.f584a.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:["main"],launchScene:"db://assets/Game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"02311",resources:"0cb06",main:"02dd2"}};
