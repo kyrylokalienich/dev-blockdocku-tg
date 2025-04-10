a5da121fb299a6c089378d0a6f0d691851f50a04
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2025-04-10 14:10:31 +0300

    update endpoint

--- src/settings.e033e.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Game.fire",orientation:"portrait",jsList:["assets/scripts/modules/crypto-js.e8870.js"],bundleVers:{internal:"02311",resources:"1093d",main:"477e4"}};
